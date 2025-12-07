ce5a343004ac83ac60344f365afa37f6837d7f2d
Jakub <pilch@student.agh.edu.pl>
2025-12-07 23:37:39 +0100

    Add simulation loop

--- simulation.py
+++ simulation.py
@@ -1,3 +1,38 @@
+import json
 from city import City
 
-City("data/city1_topography.json", "data/city1_trucks.json")
+def load_time_series(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+        raise
+    except json.JSONDecodeError as e:
+        print(f"Error: Invalid JSON format in file '{file_path}'.")
+        print(f"Details: {e}")
+        raise
+    return data['garbageSeries']
+
+
+detached_house_time_series = load_time_series("data/detached_house.json")
+apartment_building_time_series = load_time_series("data/apartment_building.json")
+public_facility_time_series = load_time_series("data/public_facility.json")
+production_plant_time_series = load_time_series("data/production_plant.json")
+
+city = City("data/city1_topography.json", "data/city1_trucks.json")
+
+running = True
+current_time = 0
+tick_duration = 15
+
+while running:
+    city.updateRubbish(detached_house_time_series,
+                        apartment_building_time_series,
+                        public_facility_time_series,
+                        production_plant_time_series,
+                        time_series_duration=672,
+                        tick_duration=tick_duration,
+                        current_time=current_time
+                        )
+    current_time += tick_duration
